01b7bee6f337714fac7ac441dd9d46dd7d44c399
priyankapatil811 <patilp8@ccs.neu.edu>
2016-03-17 10:37:06 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var app = express();
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var cookieParser  = require('cookie-parser');
+var multer = require('multer');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+
 app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-app.get('/hello', function(req, res){
-    res.send('hello world');
-});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer());
+app.use(session({ secret: process.env.PASSPORT_SECRET}));
+app.use(cookieParser(process.env.PASSPORT_SECRET));
+app.use(express.cookieSession());
+app.use(passport.initialize);
+app.use(passport.session);
+
+require("./public/assignment/server/app.js")(app);
+
 app.listen(port, ipaddress);
