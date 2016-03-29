6b1df8499052226b2c9bf98df87f78aa37bd984f
priyankapatil811 <Priyanka@Priyankas-MacBook-Pro.local>
2016-03-29 19:21:36 -0400

    adding form schema

---
+++ public/assignment/server/models/form.schema.server.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Priyanka on 3/29/16.
+ */
+module.exports = function(mongoose)
+{
+    var fieldSchema = require("./field.schema.server.js")(mongoose);
+    // use mongoose to declare a form schema
+    var form = mongoose.Schema({
+        userId: String,
+        title: String,
+        fields: [fieldSchema],
+        created: Date, //Default : Current Date
+        updated: Date  //Default : Current Date
+        // store form documents in this collection
+    }, {collection: 'cs5610.assignment.form'});
+
+    return form;
+};
