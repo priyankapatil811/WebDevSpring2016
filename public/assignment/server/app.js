a70302b051ade171e4c06eb3f2fa5763e81c6448
priyankapatil811 <Priyanka@Priyankas-MBP.cable.rcn.com>
2016-03-30 17:57:47 -0400

    using mongoose and db in form model

--- public/assignment/server/app.js
+++ public/assignment/server/app.js
@@ -9,7 +9,7 @@ module.exports = function (app,mongoose,db) {
   var userModel = require("./models/user.model.js")(mongoose,db);
   require("./services/user.service.server.js")(app,userModel);
 
-  var formModel = require("./models/form.model.js")();
+  var formModel = require("./models/form.model.js")(mongoose,db);
   require("./services/form.service.server.js")(app,userModel,formModel);
 
   var fieldModel = require("./models/fields.model.js")();
