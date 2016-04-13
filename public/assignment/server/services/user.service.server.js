68bbbb5beb0f1c4597e56c81f0bb4548044fd8fb
priyankapatil811 <Priyanka@Priyankas-MBP.cable.rcn.com>
2016-04-13 17:18:31 -0400

    added the necessary methods for security

--- public/assignment/server/services/user.service.server.js
+++ public/assignment/server/services/user.service.server.js
@@ -1,10 +1,71 @@
 /**
  * Created by Priyanka on 3/16/16.
  */
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 module.exports = function(app,userModel)
 {
+    var auth = authorized;
 
-    app.get("/api/assignment/user", function(req,res)
+    passport.use(new LocalStrategy(localStrategy));
+    passport.serializeUser(serializeUser);
+    passport.deserializeUser(deserializeUser);
+
+    app.post('/api/assignment/login', passport.authenticate('local'), login);
+    app.get("/api/assignment/user", findUser);
+    app.post("/api/assignment/user", auth,createUser);
+    app.put("/api/assignment/user/:id", auth,updateUser);
+    app.delete("/api/assignment/user/:id", auth,deleteUser);
+    app.get("/api/assignment/loggedin", loggedIn);
+    app.post("/api/assignment/logout", logout);
+
+
+    function login(req,res)
+    {
+        var user = req.user;
+        res.json(user);
+    }
+
+    function loggedIn(req, res) {
+        res.send(req.isAuthenticated() ? req.user : '0');
+        //res.json(req.session.currentUser);
+    }
+
+    function logout(req,res)
+    {
+        req.logOut();
+        res.send(200);
+    }
+
+    function localStrategy(username,password,done)
+    {
+        userModel.findUserByCredentials(username,password).then(
+            function(user)
+            {
+                console.log(user);
+                if(!user)
+                {
+                    return done(null,false);
+                }
+                else
+                {
+                    return done(null,user);
+                }
+            },
+            function(err)
+            {
+                if(err)
+                {
+                    return done(err);
+                }
+            }
+        );
+    }
+
+
+    function findUser(req,res)
     {
         var uName = req.query.username;
         var uPwd = req.query.password;
@@ -44,9 +105,9 @@ module.exports = function(app,userModel)
                 }
             );
         }
-    });
+    }
 
-    app.post("/api/assignment/user",function(req,res){
+    function createUser(req,res){
         var newUser = req.body;
         console.log(newUser);
         userModel.createUser(newUser).then(
@@ -60,9 +121,9 @@ module.exports = function(app,userModel)
                 res.status(400).send(err);
             }
         );
-    });
+    }
 
-    app.put("/api/assignment/user/:id",function(req,res){
+    function updateUser(req,res){
         var userId = req.params.id;
         var upUser = req.body;
         userModel.updateUser(userId,upUser).then(
@@ -76,9 +137,9 @@ module.exports = function(app,userModel)
                 res.status(400).send(err);
             }
         );
-    });
+    }
 
-    app.delete("/api/assignment/user/:id",function(req,res){
+    function deleteUser(req,res){
         var delUser = req.param.userId;
         userModel.deleteUserById(delUser).then(
             function(doc)
@@ -90,16 +151,37 @@ module.exports = function(app,userModel)
                 res.status(400).send(err);
             }
         );
-    });
+    }
 
-    //logged in
-    app.get("/api/assignment/loggedin", function (req, res) {
-        res.json(req.session.currentUser);
-    });
+    function serializeUser(user,done)
+    {
+        done(null,user);
+    }
 
-    app.post("/api/assignment/logout", function (req, res) {
-        req.session.destroy();
-        res.send(200);
-    });
+    function deserializeUser(user,done)
+    {
+        userModel.findUserById(user._id).then(
+            function(user)
+            {
+                done(null,user);
+            },
+            function(err)
+            {
+                done(err,null);
+            }
+        );
+    }
+
+    function authorized(req,res,next)
+    {
+        if(!req.isAuthenticated())
+        {
+            res.send(401);
+        }
+        else
+        {
+            next();
+        }
+    }
 
 };
