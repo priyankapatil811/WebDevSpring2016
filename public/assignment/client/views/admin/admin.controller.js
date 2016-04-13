1b0f920ba2a6b319cf6633d569e36214e7fcdb09
priyankapatil811 <Priyanka@Priyankas-MBP.cable.rcn.com>
2016-04-13 17:52:17 -0400

    display all users for admin and delete user

--- public/assignment/client/views/admin/admin.controller.js
+++ public/assignment/client/views/admin/admin.controller.js
@@ -1,3 +1,48 @@
 /**
  * Created by Priyanka on 2/20/16.
  */
+(function () {
+    "use strict";
+    angular
+        .module("FormBuilderApp")
+        .controller("AdminController",AdminController);
+
+    function AdminController(UserService)
+    {
+        var vm = this;
+
+        vm.users = [];
+
+        vm.init = init;
+        vm.deleteUser = deleteUser;
+
+        init();
+
+        function init()
+        {
+            UserService.findUsersByAdmin().then(
+                function(response)
+                {
+                    if(response.data)
+                    {
+                        vm.users = response.data;
+                        console.log(response.data);
+                    }
+                }
+            );
+        }
+
+        function deleteUser(user)
+        {
+            UserService.deleteUserByIdByAdmin(user._id).then(
+                function(response)
+                {
+                    if(response.data)
+                    {
+                        init();
+                    }
+                }
+            );
+        }
+    }
+})();
