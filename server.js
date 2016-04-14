c8c47c4f2c6a255c40c18d57b75845bfd4db1c07
priyankapatil811 <Priyanka@Priyankas-MBP.cable.rcn.com>
2016-04-13 21:46:53 -0400

    added security in server js

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ var mongoose = require('mongoose');
 var connectionString = 'mongodb://127.0.0.1:27017/cs5610';
 var db = mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL || connectionString);
 
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
 app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
@@ -17,8 +25,13 @@ var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer());
-app.use(session({ secret: 'priyanka'}));
-app.use(cookieParser('priyanka'));
+app.use(session(
+{
+    secret: process.env.PASSPORT_SECRET,
+    resave : true,
+    saveUninitialized : true
+}));
+app.use(cookieParser());
 app.use(passport.initialize());
 app.use(passport.session());
 
